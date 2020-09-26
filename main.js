2fe5d82e8770a0774f5e80fbc7ddccf93f31bc99
trust <trust4u@hotmail.it>
2020-09-26 16:41:51 +0200

    solved my mess

--- main.js
+++ main.js
@@ -1,7 +1,6 @@
 
 /* NavBar
 =============================================================== */
-
 let btn = document.querySelector('.menu');
 btn.onclick = function () {
     let nav = document.querySelector('.nav__list');
@@ -27,3 +26,44 @@ btn.onclick = function () {
 
 /* End__NavBar
 ==============================================================================*/
+/* TABS
+==============================================================================*/
+
+for (let i =0; i < 3; i++ ) {
+    let tabsButton = document.querySelectorAll('.tabs__button');
+    let tabsContent= document.querySelectorAll('.tabs__content');
+    tabsButton[i].addEventListener("click", function (){
+        let tabNumber = tabsButton[i].dataset.tabFor;
+        let tabToActivate = document.querySelector(`.tabs__content[data-tab-content="${tabNumber}"]`);
+/* posso usare i anche nel espressione sotto perchè è un block statement. */
+        for (let i=0; i<3; i++){
+            tabsButton[i].classList.remove('tabs__button--active');
+            tabsContent[i].classList.remove('tabs__content--active');
+        }
+        tabsButton[i].classList.add('tabs__button--active');
+        tabToActivate.classList.add('tabs__content--active');
+
+
+});
+}
+// to have a default click on my first button
+document.querySelector('.tabs__button').click();
+
+/* END TABS
+================================================================================================*/
+
+/* ACCORDION
+================================================================================================= */
+for (let i=0; i<2; i++){
+    let accordionButton = document.querySelectorAll('.accordion__button')
+accordionButton[i].addEventListener('click', function () {
+    let accordionContent = document.querySelectorAll('.accordion__content');
+    accordionContent[i].classList.toggle('accordion__content--active');
+    let accordionIconItem1 = document.querySelectorAll('.accordion__icon-item1');
+    let accordionIconItem2 = document.querySelectorAll('.accordion__icon-item2');
+    accordionIconItem1[i].classList.toggle('accordion__icon-item1--active');
+    accordionIconItem2[i].classList.toggle('accordion__icon-item2--active');
+    console.log(accordionIcon);
+ 
+})
+}
